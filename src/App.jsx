27577178b3ea2c7cb08cddfadd16102b67bd06fa
2amkoding <2amkoding@gmail.com>
2025-05-07 14:25:58 -0500

    backdrop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Hero from './components/hero/Hero'
 
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   <main>
+    <Hero />
+   </main>
   )
 }
 
