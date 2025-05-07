c4cb636bdeed3cbe714aead869f99bfb9d0f946f
2amkoding <2amkoding@gmail.com>
2025-05-07 15:12:32 -0500

    Navbar

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import Navbar from '../navbar/Navbar'
 
 function hero() {
   return (
     <div className='relative overflow-hidden min-h-[550px] sm:min-h-[660px] flex flex-col'>
-        <div className='md:h-[720px] h-[960px] md:w-[880px] right-0 bg-gradient-to-r from-orange-600 via-yellow-500 to-red-400 absolute rounded-full transform rotate-12 -top-20 shadow-[0_0_40px_rgb(225, 165, 0, 0.7)]'
+        <div className='md:h-[720px] h-[960px] md:w-[640px] w-[880px] right-0 bg-gradient-to-r from-orange-600 via-yellow-500 to-red-400 absolute rounded-full transform rotate-12 -top-20 shadow-[0_0_40px_rgb(225, 165, 0, 0.7)]'
         >
 
         </div>
+
+        <Navbar />
 
     </div>
   )
