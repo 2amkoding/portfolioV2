ce376baf87af1cd5116e559f6731021e405847e0
2amkoding <2amkoding@gmail.com>
2025-06-09 10:44:22 -0500

    rough draft layout with flex-box

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,26 @@ import Hero from './components/hero/Hero'
 
 const App = () => {
   return (
-   <main className='bg-[#0d182e]'>
-    <Hero />
-   </main>
+
+
+    <div className='bg-[#303446] min-h-screen flex p-4'>
+
+      <div className='w-1/6 border border-[#babbf1] rounded-md h-screen m-auto'>Left sidebar</div>
+
+      <div className='flex-1 flex flex-col items-center'>
+        <div className='w-2/3 border border-[#babbf1] rounded-md h-1/3 mt-8'>About Me</div>
+
+
+
+        <div className='w-2/3 border border-[#babbf1] rounded-md h-1/3 mt-8'>Insert mini-game</div>
+      </div>
+
+      <div className='w-1/6 border border-[#babbf1] rounded-md h-screen'>idk yet, stuff</div>
+
+    </div >
+
+
+
   )
 }
 
